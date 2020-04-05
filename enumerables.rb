50ada025993e369eabca2e018b7369f388f0137e
phillip musiime <phillipmusiime@gmail.com>
2020-04-05 17:15:09 +0300

    my_each done halfway. works only with arrays

---
+++ enumerables.rb
@@ -0,0 +1,49 @@
+# rubocop:disable Style/CaseEquality
+
+module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
+    i = 0
+    while i < size
+      yield(self[i])
+      i+=1
+    end
+    self
+  end
+
+  def my_each_with_index
+    
+  end
+
+  def my_select
+    
+  end
+
+  def my_all?
+    
+  end
+
+  def my_any?
+    
+  end
+
+  def my_none?
+    
+  end
+
+  def my_count
+    
+  end
+
+  def my_map
+    
+  end
+
+  def my_inject
+    
+  end
+
+  def multiply_els
+    
+  end
+end
