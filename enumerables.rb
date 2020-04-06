671e0c3e1929ede163eaad5f1bd89e53181555c8
phillip musiime <phillipmusiime@gmail.com>
2020-04-06 16:02:17 +0300

    Added my_select. Works for Arrays

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,7 @@
 module Enumerable
   def my_each
     return to_enum unless block_given?
-    
+
     for item in self
       yield item
     end
@@ -20,36 +20,38 @@ module Enumerable
   end
 
   def my_select
-    
+    return to_enum unless block_given?
+
+    selected = []
+    my_each do |item|
+      if yield(item)
+        selected.push(item)
+      end
+    end
+    selected
   end
 
   def my_all?
-    
+  
   end
 
   def my_any?
-    
   end
 
   def my_none?
-    
   end
 
   def my_count
-    
   end
 
   def my_map
-    
   end
 
   def my_inject
-    
   end
 
   def multiply_els
-    
   end
 end
 
-[5,3,2,7,"man", "dog", [6,0,9]].my_each_with_index { |ele, idx| p "#{ele} #{idx}" }
+
