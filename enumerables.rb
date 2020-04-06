1024425cb29bf9ceaf73bbe2bb9c60a632efe1c2
phillip musiime <phillipmusiime@gmail.com>
2020-04-06 11:23:39 +0300

    reconfigured my_each. now works for hashes and ranges

--- enumerables.rb
+++ enumerables.rb
@@ -1,18 +1,15 @@
-# rubocop:disable Style/CaseEquality
-
 module Enumerable
   def my_each
-    return to_enum(:my_each) unless block_given?
-    i = 0
-    while i < size
-      yield(self[i])
-      i+=1
+    return to_enum unless block_given?
+    
+    for item in self
+      yield item
     end
     self
   end
 
   def my_each_with_index
-    
+ 
   end
 
   def my_select
@@ -47,3 +44,5 @@ module Enumerable
     
   end
 end
+
+[5,3,2,7,"man", "dog", [6,0,9]].my_each_with_index { |ele, idx| p "#{ele} #{idx}" }
